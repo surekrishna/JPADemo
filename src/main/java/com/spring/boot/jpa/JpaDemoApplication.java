6f98afd4734acc67c1584e45eaf7dd95b1ad4c8e
Krishna <surekrrishna@gmail.com>
2018-08-30 23:39:59 +0530

    created user entity

--- src/main/java/com/spring/boot/jpa/JpaDemoApplication.java
+++ src/main/java/com/spring/boot/jpa/JpaDemoApplication.java
@@ -1,4 +1,4 @@
-package com.spring.boot.jpa.JPADemo;
+package com.spring.boot.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
